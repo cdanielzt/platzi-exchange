54802c87e76576f3b6cbe6f694c00799e5543bd5
cdanielzt <cdanielzt@gmail.com>
2021-10-05 18:42:57 -0500

    add icon component

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -3,6 +3,7 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <px-icon class="mr-2"/>
           <span class="font-semibold text-xl tracking-tight"
             >PlatziExchange</span
           >
@@ -23,3 +24,11 @@
     </nav>
   </header>
 </template>
+
+<script>
+import PxIcon from "@/components/PxIcon";
+export default {
+  name: "PxHeader",
+  components: { PxIcon },
+};
+</script>
