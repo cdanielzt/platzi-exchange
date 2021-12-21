eade3f372fb0e1d65e6dde864e18ef29480018d6
cdanielzt <cdanielzt@gmail.com>
2021-12-20 18:39:29 -0600

    computed property

--- src/api.js
+++ src/api.js
@@ -25,7 +25,20 @@ function getAsset(coin) {
   return getRequest(`assets/${coin}`);
 }
 
+function getAssetHistory(coin) {
+  const now = new Date();
+  const end = now.getTime();
+  now.setDate(now.getDate() - 1);
+  const start = now.getTime();
+  return fetch(
+    `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`
+  )
+    .then((res) => res.json())
+    .then((res) => res.data);
+}
+
 export default {
   getAssets,
   getAsset,
+  getAssetHistory,
 };
